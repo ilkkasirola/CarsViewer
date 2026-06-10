bd294466077b0a56f882f0c0baeaf8993c6a5fa3
Ilkka Taneli Sirola <ilkka.sirola7@gmail.com>
2026-06-10 12:18:08 +0300

    add print fot server running

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -10,6 +11,7 @@ func main() {
 	mux.HandleFunc("GET /", homeHandler)
 	mux.Handle("GET /static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	mux.HandleFunc("GET /car/{id}", carHandler)
+	fmt.Println("Server is running on http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", mux))
 
 }
